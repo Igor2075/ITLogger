efb0075593ed18f49ef7fd41278c245626cf43e7
Igor2075 <39250766+Igor2075@users.noreply.github.com>
2020-05-14 16:18:23 -0300

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from "react";
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
+import "./App.css";
+
+function App() {
+  useEffect(() => {
+    M.AutoInit();
+  });
+  return (
+    <div className="App">
+      <h1>IT Logger</h1>
+    </div>
+  );
+}
+
+export default App;
